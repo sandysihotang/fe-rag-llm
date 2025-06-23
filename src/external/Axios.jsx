a99104b1540b351765d7c915feac09816297ebc1
Sandy Sihotang <sandy.s@bytedance.com>
2025-06-23 15:07:29 +0700

    change url axios

--- src/external/Axios.jsx
+++ src/external/Axios.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const API = axios.create({
-    baseURL: "http://127.0.0.1:8000",
+    baseURL: "https://kjlmf01f-8000.asse.devtunnels.ms",
     timeout: 10000
 })
