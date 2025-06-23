f8a088bf16aac514639cf7849fbc73a4ed86c902
Sandy Sihotang <sandy.s@bytedance.com>
2025-06-23 15:25:26 +0700

    fix url

--- src/external/Axios.jsx
+++ src/external/Axios.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const API = axios.create({
-    baseURL: "https://kjlmf01f-8000.asse.devtunnels.ms",
+    baseURL: "https://kjlmf01f-8000.asse.devtunnels.ms/",
     timeout: 10000
 })
