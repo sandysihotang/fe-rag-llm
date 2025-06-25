ee4541ddad1233f3d35885c91b84275139c17822
Sandy Sihotang <sandy.s@bytedance.com>
2025-06-25 18:28:53 +0700

    http

--- src/external/Axios.jsx
+++ src/external/Axios.jsx
@@ -1,11 +1,5 @@
 import axios from "axios";
-import http from "http";
-
-const agent = new http.Agent({
-    rejectUnauthorized: false
-})
 export const API = axios.create({
     baseURL: "http://47.84.71.54:8000",
     timeout: 10000,
-    httpAgent: agent,
 })
