e9264a5e68bf3b83164bfa77deb50a9ddc4bd992
Sandy Sihotang <sandy.s@bytedance.com>
2025-06-25 15:16:06 +0700

    add agent

--- src/external/Axios.jsx
+++ src/external/Axios.jsx
@@ -1,6 +1,12 @@
 import axios from "axios";
 
+const https = require('https')
+const agent = new https.Agent({
+    rejectUnauthorized: false
+})
 export const API = axios.create({
     baseURL: "http://localhost:8000",
-    timeout: 10000
+    timeout: 10000,
+    httpAgent: agent,
+    httpsAgent: agent
 })
